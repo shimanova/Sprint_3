9efafc788eca63cca86b06be62136a6f95118e44
unknown <m5206596@gmail.com>
2026-03-22 18:17:43 +0300

    first commit

---
+++ task_1.py
@@ -0,0 +1,19 @@
+import datetime
+
+class OnlineSalesRegisterCollector:
+
+    def __init__(self):
+        self.__name_items = []
+        self.__number_items = 0
+        self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
+        self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
+
+    # Геттер для name_items
+    @property
+    def name_items(self):
+        return self.__name_items
+
+    # Геттер для number_items
+    @property
+    def number_items(self):
+        return self.__number_items
